69eb6626fcf84e019579399650985bef3254e228
xuerjun <yijunx@umich.edu>
2019-08-05 22:52:04 +0800

    end of chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'good night everyone'
+    user = {'username': 'tom'}
+    posts = [{
+        'author': {'username': 'willow'},
+        'body': 'beatiful day!'
+    }, {
+        'author': {'username': 'emily'},
+        'body': 'not beatiful day!'
+    }]
+    return render_template('index.html', title='home', user=user, posts=posts)
